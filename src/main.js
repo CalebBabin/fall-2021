c20134c74947f6a0986b85d97c3e78401994b183
calebbabin <object.caleb@gmail.com>
2021-10-24 22:04:42 -0500

    add basic low poly leaves

--- src/main.js
+++ src/main.js
@@ -112,6 +112,11 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 scene.background = new THREE.Color(0x0F1A23);
 scene.fog = new THREE.Fog(0x0F1A23, cameraDistance, cameraFar);
 
+
+const skyLight = new THREE.DirectionalLight(0xffffff, 0.5);
+skyLight.position.set(0, 1, 0);
+scene.add(skyLight);
+
 function resize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
@@ -182,7 +187,136 @@ tree.scale.setScalar(12.7);
 tree.position.x = 5;
 scene.add(tree);
 
-import foliageFrontURL from './LeavesFront.png';
+const cloudUniforms = {
+	u_time: { value: 0 },
+}
+const lowPollyFoliageMaterial = new THREE.ShaderMaterial({
+	uniforms: cloudUniforms,
+	vertexShader: document.getElementById('vertexShader').textContent,
+	fragmentShader: document.getElementById('fragmentShader').textContent
+});
+const gradColors = new Uint8Array();
+gradColors[0] = 0x97623B;
+gradColors[1] = 0xC18737;
+gradColors[2] = 0x7F4326;
+import gradUrl from './grad.png';
+const gradMap = new THREE.TextureLoader().load(gradUrl);
+gradMap.magFilter = THREE.NearestFilter;
+gradMap.minFilter = THREE.NearestFilter;
+gradMap.generateMipmaps = false;
+
+const customizedMat = new THREE.MeshToonMaterial({
+	flatShading: true,
+	color: 0xffffff,
+	gradientMap: gradMap,
+});
+customizedMat.onBeforeCompile = function (shader) {
+
+	shader.uniforms.u_time = cloudUniforms.u_time;
+
+	shader.vertexShader = `
+		uniform float u_time;
+
+		vec3 mod289(vec3 x) {
+			return x - floor(x * (1.0 / 289.0)) * 289.0;
+		}
+		vec4 mod289(vec4 x) {
+			return x - floor(x * (1.0 / 289.0)) * 289.0;
+		}
+		vec4 permute(vec4 x) {
+			return mod289(((x * 34.0) + 10.0) * x);
+		}
+		vec4 taylorInvSqrt(vec4 r) {
+			return 1.79284291400159 - 0.85373472095314 * r;
+		}
+		float snoise(vec3 v) {
+			const vec2  C = vec2(1.0 / 6.0, 1.0 / 3.0);
+			const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
+			
+			vec3 i = floor(v + dot(v, C.yyy));
+			vec3 x0 = v - i + dot(i, C.xxx);
+		
+			vec3 g = step(x0.yzx, x0.xyz);
+			vec3 l = 1.0 - g;
+			vec3 i1 = min(g.xyz, l.zxy);
+			vec3 i2 = max(g.xyz, l.zxy);
+		
+			vec3 x1 = x0 - i1 + C.xxx;
+			vec3 x2 = x0 - i2 + C.yyy; 
+			vec3 x3 = x0 - D.yyy;      
+		
+			i = mod289(i); 
+			vec4 p = permute(permute(permute( i.z + vec4(0.0, i1.z, i2.z, 1.0)) + i.y + vec4(0.0, i1.y, i2.y, 1.0)) + i.x + vec4(0.0, i1.x, i2.x, 1.0));
+		
+			float n_ = 0.142857142857; 
+			vec3  ns = n_ * D.wyz - D.xzx;
+			vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  
+			vec4 x_ = floor(j * ns.z);
+			vec4 y_ = floor(j - 7.0 * x_);    
+			vec4 x = x_ * ns.x + ns.yyyy;
+			vec4 y = y_ * ns.x + ns.yyyy;
+			vec4 h = 1.0 - abs(x) - abs(y);
+			vec4 b0 = vec4(x.xy, y.xy);
+			vec4 b1 = vec4(x.zw, y.zw);
+		
+			vec4 s0 = floor(b0) * 2.0 + 1.0;
+			vec4 s1 = floor(b1) * 2.0 + 1.0;
+			vec4 sh = -step(h, vec4(0.0));
+			vec4 a0 = b0.xzyw + s0.xzyw * sh.xxyy;
+			vec4 a1 = b1.xzyw + s1.xzyw * sh.zzww;
+			vec3 p0 = vec3(a0.xy, h.x);
+			vec3 p1 = vec3(a0.zw, h.y);
+			vec3 p2 = vec3(a1.xy, h.z);
+			vec3 p3 = vec3(a1.zw, h.w);
+		
+			vec4 norm = taylorInvSqrt(vec4(dot(p0, p0), dot(p1, p1), dot(p2, p2), dot(p3, p3)));
+				p0 *= norm.x;
+				p1 *= norm.y;
+				p2 *= norm.z;
+				p3 *= norm.w;
+				
+			vec4 m = max(0.5 - vec4(dot(x0, x0), dot(x1, x1), dot(x2, x2), dot(x3, x3)), 0.0);
+			m = m * m;
+			return 105.0 * dot(m * m, vec4(dot(p0, x0), dot(p1, x1), dot(p2, x2), dot(p3, x3)));
+		}
+		${shader.vertexShader}`;
+	shader.vertexShader = shader.vertexShader.replace(
+		'#include <begin_vertex>',
+		`
+		float bnScale = 0.7;
+		float baseNoise = snoise(vec3(position.x * bnScale + ${Math.random() * 100}, position.y * bnScale, position.z * bnScale));
+
+		float noise = snoise(vec3(position.x + u_time * 0.05, position.y, position.z));
+		float rootNoise = snoise(vec3(position.x, position.y, position.z)) * PI;
+		vec3 angle = vec3(sin(rootNoise), cos(rootNoise), 0.0);
+		vec3 transformed = position + (angle * noise * 0.1) + (baseNoise * 0.4 * vec3(sin(baseNoise * PI), cos(baseNoise * PI), cos(baseNoise * PI)));
+		`,
+	);
+
+	customizedMat.userData.shader = shader;
+
+};
+
+// Make sure WebGLRenderer doesnt reuse a single program
+
+customizedMat.customProgramCacheKey = function () {
+
+	return 69; // some random ish number
+
+};
+const lowPollyFoliage = new THREE.Mesh(
+	new THREE.IcosahedronBufferGeometry(1, 5),
+	customizedMat
+);
+lowPollyFoliage.rotation.x = 0.1;
+lowPollyFoliage.scale.y = 2;
+lowPollyFoliage.scale.z = 4;
+lowPollyFoliage.scale.x = 8;
+lowPollyFoliage.position.x = 4;
+lowPollyFoliage.position.y = 3.5;
+scene.add(lowPollyFoliage);
+
+/*import foliageFrontURL from './LeavesFront.png';
 const foliageFrontMaterial = new THREE.SpriteMaterial({
 	map: new THREE.TextureLoader().load(foliageFrontURL),
 	transparent: true,
@@ -207,7 +341,7 @@ foliageBack.scale.setScalar(30);
 foliageBack.position.x = 0;
 foliageBack.position.y = -6.5;
 foliageBack.position.z = -3;
-scene.add(foliageBack);
+scene.add(foliageBack);*/
 
 
 import leafImageURL from './leaf1.png';
@@ -245,6 +379,7 @@ function draw() {
 	if (query_vars.stats) stats.begin();
 	requestAnimationFrame(draw);
 	const delta = (Date.now() - lastFrame) / 1000;
+	cloudUniforms.u_time.value += delta;
 
 	lastFrame = Date.now();
 
@@ -296,9 +431,9 @@ function draw() {
 			emotes.velocity.y += v.y - delta * 0.5;
 			//emotes.velocity.z += v.z;
 
-			emotes.group.rotation.x = v.x * Math.PI * 10;
-			emotes.group.rotation.y = v.y * Math.PI * 10;
-			emotes.group.rotation.z = v.z * Math.PI * 10;
+			emotes.group.rotation.x = v.x * Math.PI * 20;
+			emotes.group.rotation.y = v.y * Math.PI * 20;
+			emotes.group.rotation.z = v.z * Math.PI * 20;
 
 			/*if (emotes.group.position.y > 5) {
 				emotes.velocity.y -= (emotes.group.position.y-5)/100
